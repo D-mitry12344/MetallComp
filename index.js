5f6c93b00ade583bcae15f5b9969e989f6991b54
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2023-04-10 17:27:30 +0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let Block = document.querySelector('html');
-
+let BlockHeadFirst = document.querySelector('.Head__banner_all_head');
 let headerAll = document.querySelector('.Head');
 let menuCatalog = document.querySelectorAll("#menu_catalog");
 let menuCatalogItems = document.querySelectorAll("#menu_catalog_items");
@@ -18,8 +18,20 @@ menuCatalog[1].addEventListener("click", function(){
 		menuCatalogItems[1].style.display = 'block';
 	}else{
 		menuCatalogItems[1].style.display = '';
+		
 	}
 })
+
+
+/*
+BlockHeadFirst.addEventListener('click' , function(){
+		menuCatalogItems[1].style.display = '';
+		regions.style.display = '';
+		menuCatalogItems[0].style.display = '';
+})*/
+	
+
+
 
 menuLists.forEach(function(list){
 	if(list !== menuLists[0] && list !== menuLists[1]){
@@ -31,9 +43,11 @@ menuLists.forEach(function(list){
 	list.addEventListener("click", function(){
 		if(list === menuLists[0]){
 			menuCatalogItems[1].style.display = '';
+			regions.style.display = '';
 		}
 		if(list === menuLists[1]){
 			menuCatalogItems[0].style.display = '';
+			regions.style.display = '';
 		}else{
 
 		}
@@ -84,29 +98,20 @@ const parthers = document.querySelector('.parthers');
 const menuFooter = document.querySelector('.menu-footer');
 
 const masBlocks = [catalog, bannerHead , services, stocks, news, useful, parthers, menuFooter]
-/*
-sliderItem[1].addEventListener('click',function() {
-	catalog.classList.add('Head__active_banner');
-	bannerHead.classList.remove('Head__active_banner');
-	sliderItem[1].classList.add("Head__active_slider_item");
-	sliderItem[0].classList.remove("Head__active_slider_item");
-	sliderItem.forEach(function(item){
-		item.style.borderBottom="2px solid black";
-		item.style.color="black";
+
+masBlocks.forEach(function(item){
+	item.addEventListener('click' , function(){
+			menuCatalogItems[1].style.display = '';
+			regions.style.display = '';
+			menuCatalogItems[0].style.display = '';
+	})
+	item.addEventListener('mousewheel' , function(){
+			menuCatalogItems[1].style.display = '';
+			regions.style.display = '';
+			menuCatalogItems[0].style.display = '';
 	})
 })
 
-sliderItem[0].addEventListener('click',function() {
-	bannerHead.classList.add('Head__active_banner');
-	catalog.classList.remove('Head__active_banner');
-	sliderItem[0].classList.add("Head__active_slider_item");
-	sliderItem[1].classList.remove("Head__active_slider_item");
-	sliderItem.forEach(function(item){
-		item.style.borderBottom="";
-		item.style.color="";
-	})
-})
-*/
 
 
 
@@ -116,17 +121,162 @@ if (Block.addEventListener) {
       Block.attachEvent("onmousewheel", onWheel);
 }
 
-let number=0;
+function scrolling(i,block) {
+
+		masBlocks.forEach(function(item){
+			 item.style.animation="0.3s move_banner_dis forwards";
+		})					  
+		masBlocks.forEach(function(item){
+			item.classList.remove('Head__active_banner');
+		})
+		block.classList.add('Head__active_banner');			  
+		sliderItem.forEach(function(item){
+			item.classList.remove("Head__active_slider_item");
+			item.style.borderBottom="2px solid black";
+			item.style.color="black";
+		});
+		sliderItem[i].classList.add("Head__active_slider_item");
+		block.style.animation="1s move_banner_act forwards"
+	
+	
+}						
+
+
 function onWheel(e) {
-    var delta = e.deltaY;
-    number = number + delta;
-    
+    let delta = e.deltaY;
+	//CATALOG
+    if( 
+    	delta !== 100 && sliderItem[3].classList[2] === "Head__active_slider_item" 
+    	||  delta == 100 && sliderItem[1].classList[2] === "Head__active_slider_item" 
+    ){
+    	scrolling(2,catalog)
+    }else{
+		//STOCKS
+	    if(
+	     	delta == 100 && sliderItem[0].classList[2] === "Head__active_slider_item"
+	     	||  delta !== 100 && sliderItem[2].classList[2] === "Head__active_slider_item"
+	    ){		
+	    	scrolling(1,stocks)
+		}else{
+			//FIRST BANNER DEFAULT
+				if(
+			    	delta!==100 && sliderItem[1].classList[2] === "Head__active_slider_item"
+			    	||  delta == 100 && sliderItem[7].classList[2] === "Head__active_slider_item"
+			    ){
+			    	scrolling(0,bannerHead);
+			    	sliderItem.forEach(function(item){
+						item.style.borderBottom="";
+						item.style.color="";
+					});
+			    }else{
+				    	//SERVICES		
+					if(
+				     	delta == 100 && sliderItem[2].classList[2] === "Head__active_slider_item"
+				     	||  delta !== 100 && sliderItem[4].classList[2] === "Head__active_slider_item"
+				    ){		
+				    	scrolling(3,services)
+					}else{
+					//USEFUL
+						if(
+				     	delta == 100 && sliderItem[4].classList[2] === "Head__active_slider_item"
+				     	||  delta !== 100 && sliderItem[6].classList[2] === "Head__active_slider_item"
+						){		
+							scrolling(5,useful)
+						}else{
+							//NEWS
+							 if(
+					     	delta == 100 && sliderItem[3].classList[2] === "Head__active_slider_item"
+					     	||  delta !== 100 && sliderItem[5].classList[2] === "Head__active_slider_item"
+							){		
+							 	scrolling(4,news);
+							 	sliderItem.forEach(function(item){
+									item.style.borderBottom="";
+									item.style.color="";
+								});
+							}
+							//FOOTER
+							if(
+					     	delta == 100 && sliderItem[6].classList[2] === "Head__active_slider_item"
+					     	||  delta !== 100 && sliderItem[0].classList[2] === "Head__active_slider_item"
+							){		
+								scrolling(7,menuFooter);
+								sliderItem.forEach(function(item){
+									item.style.borderBottom="";
+									item.style.color="";
+								});
+							}else{
+									//PARTHERS
+								if( 
+						     	delta == 100 && sliderItem[5].classList[2] === "Head__active_slider_item"
+						     	||  delta !== 100 && sliderItem[7].classList[2] === "Head__active_slider_item"
+								){		
+									scrolling(6,parthers)
+								}	
+							}
+							
+						
+			    }
+				}
+				}
+		}
+	}		
+}
+
+
+
+/*
+//CATALOG
+    if( 
+    	delta !== 100 && sliderItem[3].classList[2] === "Head__active_slider_item" 
+    	||  delta == 100 && sliderItem[1].classList[2] === "Head__active_slider_item" 
+    ){
+    	scrolling(2,catalog)
+    /*
+	    masBlocks.forEach(function(item){
+     		item.style.animation="0.3s move_banner_dis forwards"
+     	})
+		
+					masBlocks.forEach(function(item){
+						item.classList.remove('Head__active_banner');
+					})
+			catalog.classList.add('Head__active_banner');
+		
+		sliderItem.forEach(function(item){
+				item.classList.remove("Head__active_slider_item");
+				item.style.borderBottom="2px solid black";
+				item.style.color="black";
+		});
+		sliderItem[2].classList.add("Head__active_slider_item");
+		catalog.style.animation="1s move_banner_act forwards"
+    }else{
+//STOCKS
+	    if(
+	     	delta == 100 && sliderItem[0].classList[2] === "Head__active_slider_item"
+	     	||  delta !== 100 && sliderItem[2].classList[2] === "Head__active_slider_item"
+	    ){		
+	     		masBlocks.forEach(function(item){
+	     			 item.style.animation="0.3s move_banner_dis forwards"
+	     		})
+			    
+					masBlocks.forEach(function(item){
+						item.classList.remove('Head__active_banner');
+					})
+					stocks.classList.add('Head__active_banner');
+			    
+				sliderItem.forEach(function(item){
+					item.classList.remove("Head__active_slider_item");
+					item.style.borderBottom="2px solid black";
+					item.style.color="black";
+				});
+				sliderItem[1].classList.add("Head__active_slider_item");
+				stocks.style.animation="1s move_banner_act forwards"
+		}else{
     
 
 //FIRST BANNER DEFAULT
-	if(/*number <= 500 || number <= 0*/ 
+	if(
     	delta!==100 && sliderItem[1].classList[2] === "Head__active_slider_item"
-    	//||  delta == 100 && sliderItem[0].classList[2] === "Head__active_slider_item"
+    	||  delta == 100 && sliderItem[7].classList[2] === "Head__active_slider_item"
     ){
 	    masBlocks.forEach(function(item){
      			 item.style.animation="0.3s move_banner_dis forwards"
@@ -145,55 +295,9 @@ function onWheel(e) {
 		sliderItem[0].classList.add("Head__active_slider_item");
 		bannerHead.style.animation="1s move_banner_act forwards";
     }
-
-//CATALOG
-    if(/*number > 30 // delta==100*/  
-    	delta !== 100 && sliderItem[3].classList[2] === "Head__active_slider_item" 
-    	||  delta == 100 && sliderItem[1].classList[2] === "Head__active_slider_item" 
-    ){
-	    masBlocks.forEach(function(item){
-     		item.style.animation="0.3s move_banner_dis forwards"
-     	})
-		//setTimeout(()=>{
-					masBlocks.forEach(function(item){
-						item.classList.remove('Head__active_banner');
-					})
-			catalog.classList.add('Head__active_banner');
-		//}, 500)
-		sliderItem.forEach(function(item){
-				item.classList.remove("Head__active_slider_item");
-				item.style.borderBottom="2px solid black";
-				item.style.color="black";
-		});
-		sliderItem[2].classList.add("Head__active_slider_item");
-		catalog.style.animation="1s move_banner_act forwards"
-    }else{
-//STOCKS
-	    if(/*number <= 500 || number <= 0*/ 
-	     	delta == 100 && sliderItem[0].classList[2] === "Head__active_slider_item"
-	     	||  delta !== 100 && sliderItem[2].classList[2] === "Head__active_slider_item"
-	    ){		
-	     		masBlocks.forEach(function(item){
-	     			 item.style.animation="0.3s move_banner_dis forwards"
-	     		})
-			    //setTimeout(()=>{
-					masBlocks.forEach(function(item){
-						item.classList.remove('Head__active_banner');
-					})
-					stocks.classList.add('Head__active_banner');
-			    //}, 500)
-				sliderItem.forEach(function(item){
-					item.classList.remove("Head__active_slider_item");
-					item.style.borderBottom="2px solid black";
-					item.style.color="black";
-				});
-				sliderItem[1].classList.add("Head__active_slider_item");
-				stocks.style.animation="1s move_banner_act forwards"
-		}else{
-
 	
 //SERVICES		
-	if(/*number <= 500 || number <= 0*/ 
+	if(
      	delta == 100 && sliderItem[2].classList[2] === "Head__active_slider_item"
      	||  delta !== 100 && sliderItem[4].classList[2] === "Head__active_slider_item"
     ){		
@@ -201,12 +305,12 @@ function onWheel(e) {
      			 item.style.animation="0.3s move_banner_dis forwards";
 
      		})
-		  //setTimeout(()=>{
+		  
 					masBlocks.forEach(function(item){
 						item.classList.remove('Head__active_banner');
 					})
 				services.classList.add('Head__active_banner');
-		   // }, 500)
+		   
 			sliderItem.forEach(function(item){
 				item.classList.remove("Head__active_slider_item");
 				item.style.borderBottom="2px solid black";
@@ -220,7 +324,7 @@ function onWheel(e) {
 //USEFUL
 	
 	
-		if(/*number <= 500 || number <= 0*/ 
+		if(
      	delta == 100 && sliderItem[4].classList[2] === "Head__active_slider_item"
      	||  delta !== 100 && sliderItem[6].classList[2] === "Head__active_slider_item"
 		    ){		
@@ -228,12 +332,12 @@ function onWheel(e) {
 		     			 item.style.animation="0.3s move_banner_dis forwards";
 
 		     		})
-				  //setTimeout(()=>{
+				  
 							masBlocks.forEach(function(item){
 								item.classList.remove('Head__active_banner');
 							})
 						useful.classList.add('Head__active_banner');
-				   // }, 500)
+				   
 					sliderItem.forEach(function(item){
 						item.classList.remove("Head__active_slider_item");
 						item.style.borderBottom="2px solid black";
@@ -249,7 +353,7 @@ function onWheel(e) {
 
 
 //NEWS
-		 if(/*number <= 500 || number <= 0*/ 
+		 if(
      	delta == 100 && sliderItem[3].classList[2] === "Head__active_slider_item"
      	||  delta !== 100 && sliderItem[5].classList[2] === "Head__active_slider_item"
 		    ){		
@@ -257,12 +361,12 @@ function onWheel(e) {
 		     			 item.style.animation="0.3s move_banner_dis forwards";
 
 		     		})
-				  //setTimeout(()=>{
+				  
 							masBlocks.forEach(function(item){
 								item.classList.remove('Head__active_banner');
 							})
 						news.classList.add('Head__active_banner');
-				   // }, 500)
+				   
 					sliderItem.forEach(function(item){
 						item.classList.remove("Head__active_slider_item");
 						item.style.borderBottom="";
@@ -275,7 +379,7 @@ function onWheel(e) {
 					}
 					//FOOTER
 
-						if(/*number <= 500 || number <= 0*/ 
+						if(
 				     	delta == 100 && sliderItem[6].classList[2] === "Head__active_slider_item"
 				     	//||  delta !== 100 && sliderItem[5].classList[2] === "Head__active_slider_item"
 						    ){		
@@ -283,12 +387,12 @@ function onWheel(e) {
 						     			 item.style.animation="0.3s move_banner_dis forwards";
 
 						     		})
-								  //setTimeout(()=>{
+								  
 											masBlocks.forEach(function(item){
 												item.classList.remove('Head__active_banner');
 											})
 										menuFooter.classList.add('Head__active_banner');
-								   // }, 500)
+								   
 									sliderItem.forEach(function(item){
 										item.classList.remove("Head__active_slider_item");
 										item.style.borderBottom="";
@@ -303,7 +407,7 @@ function onWheel(e) {
 						}
 					//PARTHERS
 
-						if(/*number <= 500 || number <= 0*/ 
+						if( 
 				     	delta == 100 && sliderItem[5].classList[2] === "Head__active_slider_item"
 				     	||  delta !== 100 && sliderItem[7].classList[2] === "Head__active_slider_item"
 						    ){		
@@ -311,12 +415,12 @@ function onWheel(e) {
 						     			 item.style.animation="0.3s move_banner_dis forwards";
 
 						     		})
-								  //setTimeout(()=>{
+								  
 											masBlocks.forEach(function(item){
 												item.classList.remove('Head__active_banner');
 											})
 										parthers.classList.add('Head__active_banner');
-								   // }, 500)
+								  
 									sliderItem.forEach(function(item){
 										item.classList.remove("Head__active_slider_item");
 										item.style.borderBottom="2px solid black";
@@ -342,35 +446,4 @@ function onWheel(e) {
 
     return number;		
 }
-
-
-
-/*
-menuFooter
-
-
-let sliderItem = document.querySelectorAll(".Head__slider_item_head");
-const massivItem= [0,1,2,3,4]
-let timeBanner =0;
-
-setInterval(function(){		
-
-	let e = timeBanner+1;
-	sliderItem[timeBanner].classList.remove('Head__active_slider_item');
-	bannersHead[timeBanner].style.animation ="0.3s move_banner_dis forwards linear";
-	bannersHead[timeBanner].classList.remove('Head__active_banner');
-	if(e === 5){
-		timeBanner = -1;
-		sliderItem[0].classList.add('Head__active_slider_item');
-		bannersHead[0].classList.add('Head__active_banner');
-		bannersHead[0].style.animation ="0.3s move_banner_act forwards linear";
-		return timeBanner++;	
-	}
-	sliderItem[e].classList.add('Head__active_slider_item');	
-	bannersHead[e].classList.add('Head__active_banner');
-	bannersHead[e].style.animation ="0.3s move_banner_act forwards linear";	
-	return timeBanner++;
-}, 5000)
-		
-
 */
